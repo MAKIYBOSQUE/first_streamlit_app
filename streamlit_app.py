9d0a14dbeed39989953d120b0a07ac758c7e1dd8
MAKIYBOSQUE <142950109+MAKIYBOSQUE@users.noreply.github.com>
2023-08-23 11:27:51 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Bacon & Egg')
+streamlit.text('Tapsilog')
+streamlit.text('Longsilog')
