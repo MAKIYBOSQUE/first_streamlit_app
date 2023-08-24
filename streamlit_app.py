8ead1c9f4c50de00dd04de66f23e0decfce9853e
MAKIYBOSQUE <142950109+MAKIYBOSQUE@users.noreply.github.com>
2023-08-24 17:39:34 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,19 @@ try:
 except URLError as e:
     streamlit.error()
 
-streamlit.stop()
+
+streamlit.header("Fruityvice Fruit Advice!!!")
+def get_fruit_data_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+
+if streamlit.button('Get fruit load list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_data_list()
+    streamlit.dataframe(my_data_rows)
+
+# streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
