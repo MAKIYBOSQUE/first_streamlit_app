4c9d21be37107378975cefe467df33c2d8eb4650
MAKIYBOSQUE <142950109+MAKIYBOSQUE@users.noreply.github.com>
2023-08-24 10:37:27 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# write your own comment - what does this do?s
 streamlit.dataframe(fruityvice_normalized)
